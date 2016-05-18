83e68df6c99bddb0972574230cdc804a9f91eadf
Will Sokolowski <sokos6@Wills-MacBook-Pro.local>
2016-05-18 18:07:47 -0400

    datepicker ready

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ionic-datepicker'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,4 +26,31 @@ angular.module('starter', ['ionic'])
   $scope.submit = function(username) {
     alert("Thanks " + username);
   }
+}).controller('HomeCtrl', function ($scope, ionicDatePicker) {
+
+    var ipObj1 = {
+        callback: function (val) {  //Mandatory
+            console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+        },
+        disabledDates: [            //Optional
+            new Date(2016, 2, 16),
+            new Date(2015, 3, 16),
+            new Date(2015, 4, 16),
+            new Date(2015, 5, 16),
+            new Date('Wednesday, August 12, 2015'),
+            new Date("08-16-2016"),
+            new Date(1439676000000)
+        ],
+        from: new Date(2012, 1, 1), //Optional
+        to: new Date(2016, 10, 30), //Optional
+        inputDate: new Date(),      //Optional
+        mondayFirst: true,          //Optional
+        disableWeekdays: [0],       //Optional
+        closeOnSelect: false,       //Optional
+        templateType: 'popup'       //Optional
+    };
+
+    $scope.openDatePicker = function(){
+        ionicDatePicker.openDatePicker(ipObj1);
+    };
 });
